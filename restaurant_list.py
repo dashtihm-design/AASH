0ceba4fe42f27b4ec8c354d1a0ebc2fd44151ad4
dashtihm-design <dashtihm@gmail.com>
2025-12-21 19:28:25 +0300

    Refactor restaurant choices to structured format

--- restaurant_list.py
+++ restaurant_list.py
@@ -1,32 +1,183 @@
 choices = [
-    "Swaikhat  https://maps.app.goo.gl/EjE9bMXvsfpjJGdh7",
-    "Amman Bhelpuri  https://maps.app.goo.gl/4N9vGAfdzQwpfbLcA",
-    "Ghadeer Al Bahrain  https://maps.app.goo.gl/naAnZuPffcDbJFNcA",
-    "Al Sayed Lebanese Restaurant  https://maps.app.goo.gl/QXFPHig1t5g1WKkL9",
-    "Tampopo - Ramen Shop  https://maps.app.goo.gl/Y8wEdPwAt54cw2Ns6",
-    "Meme's Curry  https://maps.app.goo.gl/bJrfdVByaQHjoKQR9",
-    "Naranj  https://maps.app.goo.gl/eopzWbfrm43XYiYT6",
-    "Elamigos Restaurant Ras  https://maps.app.goo.gl/cXHTsttqNP28YS1BA",
-    "Midar  https://maps.app.goo.gl/gghCfL6tYemQ8pmE8",
-    "Cantina  https://maps.app.goo.gl/Qqd3ZUwi91YVPEkc8",
-    "San Ristorante  https://maps.app.goo.gl/C6qtozPRTRrbK1Mv7",
-    "Agnii (The Avenues)  https://maps.app.goo.gl/3XN9fQtzgX6tw6kK9",
-    "Copper Chimney - Indian Restaurant  https://maps.app.goo.gl/7e9Ra34a77Uybjep6",
-    "Matbakhi by Sawsan  https://maps.app.goo.gl/ZfbJceGMzsic2PBP9",
-    "Principale Ristorante Di Nino  https://maps.app.goo.gl/PnEEHf3amn3CzKhM9",
-    "Assaha Restaurant  https://maps.app.goo.gl/jA1ACZqeAETPxoGo8",
-    "The Sandwich Shop https://maps.app.goo.gl/2xarP2AiJdGYj5HZ6",
-    "Seoulian  https://maps.app.goo.gl/67ny89t2t7LyxwDLA",
-    "SOLO Pizza Napulitana https://maps.app.goo.gl/zXWYVk3ZWmshtUwZ6",
-    "Zlatni Juli  https://maps.app.goo.gl/iQU8uiWr3KjTuQhVA",
-    "Vigonovo https://maps.app.goo.gl/fY4vrJVqJzHZFTCo8",
-    "Tekka Lumee  https://maps.app.goo.gl/21TdcUzF254amC518",
-    "Pinolo Ristorante  https://maps.app.goo.gl/P8Hnu1ZLMgcBtdCi8",
-    "Ori Omakase Sushi  https://maps.app.goo.gl/KeVJ7aAvezaJyB5TA",
-    "BARBA  https://maps.app.goo.gl/YPsUY4tyevBsBjpA8",
-    "San Opera  https://maps.app.goo.gl/5RG7jQ4gr1pBkKEx5",
-    "YUKAI  https://maps.app.goo.gl/KHmomeH6hVzbjXY59",
-    "Giulia by Vigonovo  https://maps.app.goo.gl/aPxjqc7ggnX2LPSU9",
-    "Folio Bistro & Bakery  https://maps.app.goo.gl/kTinT9YhG5RDnUnC8",
-    "Wok n roll  https://maps.app.goo.gl/pK2higCjHUxskyoe6"
+    {
+        "name": "Swaikhat",
+        "map_url": "https://maps.app.goo.gl/EjE9bMXvsfpjJGdh7",
+        "embed_url": "https://www.google.com/maps?q=Swaikhat&output=embed",
+        "images": ["https://images.unsplash.com/photo-1555396273-367ea4eb4db5"]
+    },
+    {
+        "name": "Amman Bhelpuri",
+        "map_url": "https://maps.app.goo.gl/4N9vGAfdzQwpfbLcA",
+        "embed_url": "https://www.google.com/maps?q=Amman+Bhelpuri&output=embed",
+        "images": ["https://images.unsplash.com/photo-1604908177522-429d2cbb4a30"]
+    },
+    {
+        "name": "Ghadeer Al Bahrain",
+        "map_url": "https://maps.app.goo.gl/naAnZuPffcDbJFNcA",
+        "embed_url": "https://www.google.com/maps?q=Ghadeer+Al+Bahrain&output=embed",
+        "images": ["https://images.unsplash.com/photo-1540189549336-e6e99c3679fe"]
+    },
+    {
+        "name": "Al Sayed Lebanese Restaurant",
+        "map_url": "https://maps.app.goo.gl/QXFPHig1t5g1WKkL9",
+        "embed_url": "https://www.google.com/maps?q=Al+Sayed+Lebanese+Restaurant&output=embed",
+        "images": ["https://images.unsplash.com/photo-1600891964599-f61ba0e24092"]
+    },
+    {
+        "name": "Tampopo - Ramen Shop",
+        "map_url": "https://maps.app.goo.gl/Y8wEdPwAt54cw2Ns6",
+        "embed_url": "https://www.google.com/maps?q=Tampopo+Ramen+Shop&output=embed",
+        "images": ["https://images.unsplash.com/photo-1589302168068-964664d93dc0"]
+    },
+    {
+        "name": "Meme's Curry",
+        "map_url": "https://maps.app.goo.gl/bJrfdVByaQHjoKQR9",
+        "embed_url": "https://www.google.com/maps?q=Meme's+Curry&output=embed",
+        "images": ["https://images.unsplash.com/photo-1604908554161-5e0c1c4c9d65"]
+    },
+    {
+        "name": "Naranj",
+        "map_url": "https://maps.app.goo.gl/eopzWbfrm43XYiYT6",
+        "embed_url": "https://www.google.com/maps?q=Naranj+Restaurant&output=embed",
+        "images": ["https://images.unsplash.com/photo-1600891964311-7d42b37f43ad"]
+    },
+    {
+        "name": "Elamigos Restaurant Ras",
+        "map_url": "https://maps.app.goo.gl/cXHTsttqNP28YS1BA",
+        "embed_url": "https://www.google.com/maps?q=Elamigos+Restaurant+Ras&output=embed",
+        "images": ["https://images.unsplash.com/photo-1625944525533-473f1a3a4f34"]
+    },
+    {
+        "name": "Midar",
+        "map_url": "https://maps.app.goo.gl/gghCfL6tYemQ8pmE8",
+        "embed_url": "https://www.google.com/maps?q=Midar+Restaurant&output=embed",
+        "images": ["https://images.unsplash.com/photo-1605478371208-1c0e2f3f5e7e"]
+    },
+    {
+        "name": "Cantina",
+        "map_url": "https://maps.app.goo.gl/Qqd3ZUwi91YVPEkc8",
+        "embed_url": "https://www.google.com/maps?q=Cantina+Restaurant&output=embed",
+        "images": ["https://images.unsplash.com/photo-1525755662778-989d0524087e"]
+    },
+    {
+        "name": "San Ristorante",
+        "map_url": "https://maps.app.goo.gl/C6qtozPRTRrbK1Mv7",
+        "embed_url": "https://www.google.com/maps?q=San+Ristorante&output=embed",
+        "images": ["https://images.unsplash.com/photo-1600891965050-36c3d7e3f1d3"]
+    },
+    {
+        "name": "Agnii (The Avenues)",
+        "map_url": "https://maps.app.goo.gl/3XN9fQtzgX6tw6kK9",
+        "embed_url": "https://www.google.com/maps?q=Agnii+The+Avenues&output=embed",
+        "images": ["https://images.unsplash.com/photo-1601050690597-df0568f70950"]
+    },
+    {
+        "name": "Copper Chimney - Indian Restaurant",
+        "map_url": "https://maps.app.goo.gl/7e9Ra34a77Uybjep6",
+        "embed_url": "https://www.google.com/maps?q=Copper+Chimney+Indian+Restaurant&output=embed",
+        "images": ["https://images.unsplash.com/photo-1589308078054-83273b6d94c2"]
+    },
+    {
+        "name": "Matbakhi by Sawsan",
+        "map_url": "https://maps.app.goo.gl/ZfbJceGMzsic2PBP9",
+        "embed_url": "https://www.google.com/maps?q=Matbakhi+by+Sawsan&output=embed",
+        "images": ["https://images.unsplash.com/photo-1600891963925-cfb4c3e2f4b5"]
+    },
+    {
+        "name": "Principale Ristorante Di Nino",
+        "map_url": "https://maps.app.goo.gl/PnEEHf3amn3CzKhM9",
+        "embed_url": "https://www.google.com/maps?q=Principale+Ristorante+Di+Nino&output=embed",
+        "images": ["https://images.unsplash.com/photo-1529042410759-befb1204b468"]
+    },
+    {
+        "name": "Assaha Restaurant",
+        "map_url": "https://maps.app.goo.gl/jA1ACZqeAETPxoGo8",
+        "embed_url": "https://www.google.com/maps?q=Assaha+Restaurant&output=embed",
+        "images": ["https://images.unsplash.com/photo-1540189549336-e6e99c3679fe"]
+    },
+    {
+        "name": "The Sandwich Shop",
+        "map_url": "https://maps.app.goo.gl/2xarP2AiJdGYj5HZ6",
+        "embed_url": "https://www.google.com/maps?q=The+Sandwich+Shop&output=embed",
+        "images": ["https://images.unsplash.com/photo-1528735602780-2552fd46c7af"]
+    },
+    {
+        "name": "Seoulian",
+        "map_url": "https://maps.app.goo.gl/67ny89t2t7LyxwDLA",
+        "embed_url": "https://www.google.com/maps?q=Seoulian+Restaurant&output=embed",
+        "images": ["https://images.unsplash.com/photo-1604908554161-5e0c1c4c9d65"]
+    },
+    {
+        "name": "SOLO Pizza Napulitana",
+        "map_url": "https://maps.app.goo.gl/zXWYVk3ZWmshtUwZ6",
+        "embed_url": "https://www.google.com/maps?q=SOLO+Pizza+Napulitana&output=embed",
+        "images": ["https://images.unsplash.com/photo-1548365328-8b849e6a92b5"]
+    },
+    {
+        "name": "Zlatni Juli",
+        "map_url": "https://maps.app.goo.gl/iQU8uiWr3KjTuQhVA",
+        "embed_url": "https://www.google.com/maps?q=Zlatni+Juli&output=embed",
+        "images": ["https://images.unsplash.com/photo-1600891964092-4316c288032e"]
+    },
+    {
+        "name": "Vigonovo",
+        "map_url": "https://maps.app.goo.gl/fY4vrJVqJzHZFTCo8",
+        "embed_url": "https://www.google.com/maps?q=Vigonovo+Restaurant&output=embed",
+        "images": ["https://images.unsplash.com/photo-1529042410759-befb1204b468"]
+    },
+    {
+        "name": "Tekka Lumee",
+        "map_url": "https://maps.app.goo.gl/21TdcUzF254amC518",
+        "embed_url": "https://www.google.com/maps?q=Tekka+Lumee&output=embed",
+        "images": ["https://images.unsplash.com/photo-1579584425555-c3ce17fd4351"]
+    },
+    {
+        "name": "Pinolo Ristorante",
+        "map_url": "https://maps.app.goo.gl/P8Hnu1ZLMgcBtdCi8",
+        "embed_url": "https://www.google.com/maps?q=Pinolo+Ristorante&output=embed",
+        "images": ["https://images.unsplash.com/photo-1600891965050-36c3d7e3f1d3"]
+    },
+    {
+        "name": "Ori Omakase Sushi",
+        "map_url": "https://maps.app.goo.gl/KeVJ7aAvezaJyB5TA",
+        "embed_url": "https://www.google.com/maps?q=Ori+Omakase+Sushi&output=embed",
+        "images": ["https://images.unsplash.com/photo-1579871494447-9811cf80d66c"]
+    },
+    {
+        "name": "BARBA",
+        "map_url": "https://maps.app.goo.gl/YPsUY4tyevBsBjpA8",
+        "embed_url": "https://www.google.com/maps?q=BARBA+Restaurant&output=embed",
+        "images": ["https://images.unsplash.com/photo-1600891964599-f61ba0e24092"]
+    },
+    {
+        "name": "San Opera",
+        "map_url": "https://maps.app.goo.gl/5RG7jQ4gr1pBkKEx5",
+        "embed_url": "https://www.google.com/maps?q=San+Opera+Restaurant&output=embed",
+        "images": ["https://images.unsplash.com/photo-1525755662778-989d0524087e"]
+    },
+    {
+        "name": "YUKAI",
+        "map_url": "https://maps.app.goo.gl/KHmomeH6hVzbjXY59",
+        "embed_url": "https://www.google.com/maps?q=YUKAI+Restaurant&output=embed",
+        "images": ["https://images.unsplash.com/photo-1589302168068-964664d93dc0"]
+    },
+    {
+        "name": "Giulia by Vigonovo",
+        "map_url": "https://maps.app.goo.gl/aPxjqc7ggnX2LPSU9",
+        "embed_url": "https://www.google.com/maps?q=Giulia+by+Vigonovo&output=embed",
+        "images": ["https://images.unsplash.com/photo-1529042410759-befb1204b468"]
+    },
+    {
+        "name": "Folio Bistro & Bakery",
+        "map_url": "https://maps.app.goo.gl/kTinT9YhG5RDnUnC8",
+        "embed_url": "https://www.google.com/maps?q=Folio+Bistro+Bakery&output=embed",
+        "images": ["https://images.unsplash.com/photo-1504754524776-8f4f37790ca0"]
+    },
+    {
+        "name": "Wok n roll",
+        "map_url": "https://maps.app.goo.gl/pK2higCjHUxskyoe6",
+        "embed_url": "https://www.google.com/maps?q=Wok+n+roll+Restaurant&output=embed",
+        "images": ["https://images.unsplash.com/photo-1604908177522-429d2cbb4a30"]
+    }
 ]
+
